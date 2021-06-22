2bf259a711041e118b05cc099b8ed7b9c6e5d74d
Ahlam-M <ahlam.az.mhmd@gmail.com>
2021-06-22 19:08:11 +0300

    control toastjam show() and apearance

--- app/src/main/java/com/ahlam/toastjam_sample/MainActivity.java
+++ app/src/main/java/com/ahlam/toastjam_sample/MainActivity.java
@@ -1,8 +1,6 @@
 package com.ahlam.toastjam_sample;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import com.ahlam.toastjam.entities.TGravity;
 import com.ahlam.toastjam.entities.TShape;
@@ -10,22 +8,16 @@ import com.ahlam.toastjam.ToastJam;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
             ToastJam.setup(this, "Hi there !")
                     .setGravity(TGravity.TOP)
-                    //.setShape(TShape.RAWNED)
-                    .setTextColor(getResources().getColor(R.color.background_color))
-                    .setBackgroundColor(R.color.colorAccent)
+                    .setShape(TShape.RAWNED)
+                    .setTextColor(getResources().getColor(R.color.colorAccent))
+                    .setToastJamColor(getResources().getColor(R.color.colorPrimary))
                     .show();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
     }
 }
--- toastjam/src/main/java/com/ahlam/toastjam/ToastJam.java
+++ toastjam/src/main/java/com/ahlam/toastjam/ToastJam.java
@@ -4,18 +4,27 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.Drawable;
 import android.text.PrecomputedText;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
+import androidx.annotation.GravityInt;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.drawable.DrawableCompat;
 
 import com.ahlam.toastjam.entities.TGravity;
@@ -31,17 +40,26 @@ public class ToastJam {
     @SuppressLint("StaticFieldLeak")
     private static ToastJam toastjam;
     private final TextView view;
+    private final LinearLayout parent;
 
     private final Context context;
+
     private @ColorInt int background_color;
-    private TShape tshape;
+    private @ColorInt int text_color;
+    private @DrawableRes int tshape;
+    private @GravityInt int tgravity;
+
 
     private ToastJam(Context context){
         this.context = context;
-        view = (TextView) View.inflate(context, R.layout.toastjam_layout, null);
+        parent = (LinearLayout) View.inflate(context, R.layout.toastjam_layout, null);
+        view = (TextView) parent.findViewById(R.id.toastjam);
 
-        background_color = -867414964;//transparent gray
-        tshape = TShape.ROUND_SQUARE;
+        //default values
+        background_color = ContextCompat.getColor(context, R.color.background_color); //-867414964;//transparent gray
+        text_color = Color.WHITE;
+        tshape = R.drawable.round_square;
+        tgravity = Gravity.BOTTOM;
     }
 
 
@@ -52,67 +70,49 @@ public class ToastJam {
     }
 
     public ToastJam setGravity(TGravity gravity){
+        try {
         switch (gravity){
             case TOP:
-                toastjam.view.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL);
+                tgravity = Gravity.TOP;
                 break;
             case CENTER:
-                toastjam.view.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL);
+                tgravity = Gravity.CENTER_VERTICAL;
                 break;
-
             default:
-                toastjam.view.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL);
+                tgravity = Gravity.BOTTOM;
                 break;
         }
-        return toastjam;
-    }
-
-
-    public ToastJam setBackgroundColor(@ColorInt int color){
-
-        background_color = color;
-
-        /*try {
-            view.setBackgroundColor(color);
-        } catch (NullPointerException e){
-            e.printStackTrace();
-        }*/
-
-        try {
-            Drawable unwrappedDrawable = toastjam.view.getBackground();
-            Drawable wrappedDrawable = DrawableCompat.wrap(unwrappedDrawable);
-            DrawableCompat.setTint(wrappedDrawable, color);
-            toastjam.view.setBackground(wrappedDrawable);
-
-        } catch (NullPointerException e) {
+        } catch (Exception e){
             e.printStackTrace();
         }
         return toastjam;
     }
 
+
+    public ToastJam setToastJamColor(@ColorInt int color){
+        try{
+            background_color = color;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return toastjam;
+    }
 
 
     public ToastJam setShape(TShape shape){
-
         try {
             switch (shape) {
                 case SQUARE:
-                    toastjam.view.setBackground(context.getResources().getDrawable(R.drawable.square));
-                    toastjam.view.setPadding(50, 30, 50, 30);
-                    break;
-                case ROUND_SQUARE:
-                    toastjam.view.setBackground(context.getResources().getDrawable(R.drawable.round_square));
-                    toastjam.view.setPadding(50, 30, 50, 30);
+                    tshape = R.drawable.square;
                     break;
                 case RAWNED:
-                    toastjam.view.setBackground(context.getResources().getDrawable(R.drawable.rawned));
-                    toastjam.view.setPadding(50, 30, 50, 30);
+                    tshape = R.drawable.rawned;
                     break;
                 default:
+                    tshape = R.drawable.round_square;
                     break;
             }
-            setBackgroundColor(background_color);
-        } catch (NullPointerException e){
+        } catch (Exception e){
             e.printStackTrace();
         }
         return toastjam;
@@ -120,32 +120,44 @@ public class ToastJam {
 
     public ToastJam setTextColor(@ColorInt int color){
         try{
-            toastjam.view.setTextColor(color);
-        } catch (NullPointerException e){
+            text_color = color;
+        } catch (Exception e){
             e.printStackTrace();
         }
         return toastjam;
     }
 
-    public void show() throws InterruptedException {
+    private Drawable prepareBackground(){
 
-        RelativeLayout relativeLayout = new RelativeLayout(context);
+        try {
+            Drawable unwrappedDrawable = AppCompatResources.getDrawable(context, tshape);
+            Drawable wrappedDrawable = DrawableCompat.wrap(unwrappedDrawable);
+            wrappedDrawable.setColorFilter(new PorterDuffColorFilter(background_color,PorterDuff.Mode.SRC_IN));
+            //DrawableCompat.setTint(wrappedDrawable, background_color);
+            return wrappedDrawable;
+            //toastjam.view.getRootView().setBackground(wrappedDrawable);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
-        ((Activity) context).addContentView(view, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return context.getResources().getDrawable(R.drawable.round_square);
+    }
 
-        Thread.sleep(2);
-        //((ViewGroup) ((Activity) context).getWindow().getDecorView()).removeView(view);
+    public void show() {
 
-        //((Activity) context).getParent().getWindowManager().removeView(view);
-        WindowManager windowManager = (WindowManager) ((Activity) context).getSystemService(WINDOW_SERVICE);
-//        windowManager.removeView(view);
+        //create toastjam
+        toastjam.view.setGravity(Gravity.CENTER);
+        toastjam.view.setPadding(50, 30, 50, 30);
+        toastjam.view.setTextColor(text_color);
+        toastjam.view.setBackground(prepareBackground());
 
 
+        ViewGroup rootview = (ViewGroup) ((Activity) context).findViewById(android.R.id.content);
+        parent.setGravity(tgravity|Gravity.CENTER_HORIZONTAL);
+        rootview.addView(parent);
+    }
 
-        //((Activity) context).addContentView(relativeLayout, new WindowManager.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        //WindowManager windowManager = (WindowManager) ((Activity) context).getSystemService(WINDOW_SERVICE);
-        //windowManager.addView(view, new WindowManager.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+    private void hideToastJam(){
+
     }
 }
